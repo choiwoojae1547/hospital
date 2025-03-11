4f3067bcbee80855cd134a0511c584ee6eb2b926
muhancloud <cwj1547@gmail.com>
2025-03-11 13:34:22 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,44 +5,6 @@ $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
 
-try {
-    // DB에 연결
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // 연결 성공 메시지 출력
-    echo "Connection successful!<br>";
-
-    // SQL 쿼리 작성: hospital_db 테이블의 모든 데이터를 조회
-    $sql = "SELECT * FROM reservations"; // reservations 테이블에서 모든 데이터를 조회
-
-    // 쿼리 실행
-    $stmt = $pdo->query($sql);
-
-    // 결과 출력
-    if ($stmt) {
-        echo "<h3>Hospital DB Table Contents:</h3><table border='1'><tr><th>ID</th><th>Name</th><th>Specialty</th></tr>";
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<tr><td>" . $row['id'] . "</td><td>" . $row['name'] . "</td><td>" . $row['specialty'] . "</td></tr>";
-        }
-        echo "</table>";
-    } else {
-        echo "No data found.";
-    }
-
-} catch (PDOException $e) {
-    // 연결 실패 시 오류 메시지 출력
-    echo "DB 연결 실패: " . $e->getMessage();
-}
-?>
-
-<?php
-// 환경 변수로 DB 연결 정보 설정
-$host = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$dbname = getenv('DB_NAME');
-
 // Redis 클라이언트 생성
 $redis = new Redis();
 $redisHost = getenv('REDIS_HOST');
@@ -53,7 +15,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // 연결 성공 메시지 출력
+    // DB 연결 성공 메시지 출력
     echo "Connection to DB successful!<br>";
 
     // Redis 서버에 연결
